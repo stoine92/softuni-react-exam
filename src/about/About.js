751e77f2893222c3af5f17cd460792486ccf59c0
Stoyan Nikolov <tical9212@gmail.com>
2021-04-04 14:18:53 +0100

    About page done

--- src/about/About.js
+++ src/about/About.js
@@ -10,27 +10,22 @@ function About(props) {
 
       <Header />
       <Jumbotron>
-        <h1>About page</h1>
+        <h3>My mission</h3>
         <p>
-          Contrary to popular belief, Lorem Ipsum is not simply random text. It
-          has roots in a piece of classical Latin literature from 45 BC, making
-          it over 2000 years old. Richard McClintock, a Latin professor at
-          Hampden-Sydney College in Virginia, looked up one of the more obscure
-          Latin words, consectetur, from a Lorem Ipsum passage, and going
-          through the cites of the word in classical literature, discovered the
-          undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-          1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
-          Evil) by Cicero, written in 45 BC. This book is a treatise on the
-          theory of ethics, very popular during the Renaissance. The first line
-          of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in
-          section 1.10.32. The standard chunk of Lorem Ipsum used since the
-          1500s is reproduced below for those interested. Sections 1.10.32 and
-          1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also
-          reproduced in their exact original form, accompanied by English
-          versions from the 1914 translation by H. Rackham.
+          This is Stoyan's first project with ReactJS assigned by Softuni Sofia. The mission of this website is to provide some information for the Star Wars Movies and its content. In this site you can find useful information about heroes, planets, starships and the actual films. There also is a small quiz for the biggest fans to test their knowledge about the movies and give themselves a bit of a challenge ! 
+        </p>
+        <h3>My story</h3>
+        <p>
+          I started studying in Softuni Sofia in January 2020, as I applied for the free JavaScript basics course provided by them. As putting hard work into account I had the chance to continue with the Fundamentals, Advanced course and now with their ReactJS course. Throughout the journey I can proudly say I have learnt a new skill and hopefully soon can name myself as a FrontEnd Developer.
+        </p>
+        <h3>Contact</h3>
+        <p>
+          Stoyan Nikolov <br />
+          Tel: +44 7843 138809 <br />
+          Email: tical9212@gmail.com
         </p>
         <p>
-          <Button variant="primary" href="/">Go back</Button>
+          <Button preventDefault variant="primary" href="/mainPage/MainPage">Go back</Button>
         </p>
       </Jumbotron>
       <Footer />
