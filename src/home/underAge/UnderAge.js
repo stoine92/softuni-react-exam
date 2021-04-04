77a69aef0a78b34830e1e7751b0c6e530fbeb650
Stoyan Nikolov <tical9212@gmail.com>
2021-04-04 14:54:53 +0100

    footer removed from underage page

--- src/home/underAge/UnderAge.js
+++ src/home/underAge/UnderAge.js
@@ -8,7 +8,6 @@ function UnderAge() {
       <h1 className="denied-h1">
         Access Denied! Sorry, you need to be 16 years old or over to join.
       </h1>
-      <Footer />
     </div>
   );
 }
